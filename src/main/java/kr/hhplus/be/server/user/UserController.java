d093e1c98a8e6217206267f392e4f70366f38804
Jihye Oh <jihyegnb@naver.com>
2025-06-05 21:14:59 +0900

    feat: add user domain

---
+++ src/main/java/kr/hhplus/be/server/user/UserController.java
@@ -0,0 +1,36 @@
+package kr.hhplus.be.server.user;
+
+import java.math.BigDecimal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  public record ChargeRequest(
+    Long userId,
+    BigDecimal price
+  ) {}
+
+  public record ChargeResponse(
+    Long userId,
+    BigDecimal charge
+  ) {}
+
+  @PostMapping("/{id}/chargeBalance")
+  public ResponseEntity<ChargeResponse> chargeBalance(
+    @PathVariable Long id,
+    @RequestBody ChargeRequest request
+) {
+    BigDecimal updatedBalance = userService.chargeBalance(id, request.price());
+    return ResponseEntity.ok(new ChargeResponse(id, updatedBalance));
+  }
+}
